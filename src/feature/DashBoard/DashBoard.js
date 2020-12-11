dd5d4aa061e7de930e2f283327071ac6c37b466f
Thinhnv <thinhnv@gviet.vn>
2020-12-11 10:34:34 +0700

    Add function login logout

--- src/feature/DashBoard/DashBoard.js
+++ src/feature/DashBoard/DashBoard.js
@@ -1,11 +1,10 @@
-import React from "react";
-import { Route, Switch } from "react-router-dom";
-import Courses from "../Cousers";
-import Home from "../Home";
-import Login from "../Login";
-import Footer from "./Footer";
-import Menu from "./Menu";
-import Register from "../Register";
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Courses from '../Cousers';
+import Home from '../Home';
+import Login from '../Login';
+import Footer from './Footer';
+import Menu from './Menu';
 
 function DashBoard(props) {
   return (
@@ -13,16 +12,10 @@ function DashBoard(props) {
       <Menu />
 
       <Switch>
-        <Route exact path="/">
+        <Route exact path='/'>
           <Home />
         </Route>
-        <Route path="/login">
-          <Login />
-        </Route>
-        <Route path="/register">
-          <Register />
-        </Route>
-        <Route path="/courses">
+        <Route exact path='/courses'>
           <Courses />
         </Route>
       </Switch>
--- src/feature/Login/Login.js
+++ src/feature/Login/Login.js
@@ -1,56 +1,92 @@
-import React from "react";
-import "./login.css";
-
+import React, { useState, useEffect } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import './login.css';
+import { useRecoilState } from 'recoil';
+import { loginState } from '../../recoil/appState';
 const Login = (props) => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [login, setLogin] = useRecoilState(loginState);
+  let history = useHistory();
+  const onChange = (type, e) => {
+    type(e.target.value);
+    console.log(username);
+    console.log(password);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    if (username === 'admin' && password === 'admin') {
+      localStorage.setItem('accessToken', username);
+      sessionStorage.setItem('accessToken', username);
+      localStorage.setItem('user', JSON.stringify({ name: username }));
+      sessionStorage.setItem('user', JSON.stringify({ name: username }));
+      setLogin(true);
+      history.push('/');
+    }
+  };
+
+  useEffect(() => {
+    console.log(login);
+    if (login) {
+      history.push('/');
+    }
+    return () => {
+      console.log('unmout');
+    };
+  }, []);
   return (
-    <div className="login">
-      <div className="container-login100">
-        <div className="wrap-login100" style={{ justifyContent: "center" }}>
+    <div className='login'>
+      <div className='container-login100'>
+        <div className='wrap-login100' style={{ justifyContent: 'center' }}>
           <div>
-            <form className="login100-form validate-form">
-              <span className="login100-form-title">Member Login</span>
+            <form className='login100-form validate-form' onSubmit={onSubmit}>
+              <span className='login100-form-title'>Đăng nhập</span>
               <div
-                className="wrap-input100 validate-input"
-                data-validate="Valid email is required: ex@abc.xyz"
+                className='wrap-input100 validate-input'
+                data-validate='Valid email is required: ex@abc.xyz'
               >
                 <input
-                  className="input100"
-                  type="text"
-                  name="email"
-                  placeholder="Email"
+                  className='input100'
+                  type='text'
+                  name='username'
+                  placeholder='Tài khoản'
+                  value={username}
+                  onChange={(e) => onChange(setUsername, e)}
                 />
-                <span className="focus-input100" />
-                <span className="symbol-input100">
-                  <i className="fa fa-envelope" aria-hidden="true" />
+                <span className='focus-input100' />
+                <span className='symbol-input100'>
+                  <i className='fa fa-envelope' aria-hidden='true' />
                 </span>
               </div>
               <div
-                className="wrap-input100 validate-input"
-                data-validate="Password is required"
+                className='wrap-input100 validate-input'
+                data-validate='Password is required'
               >
                 <input
-                  className="input100"
-                  type="password"
-                  name="pass"
-                  placeholder="Password"
+                  className='input100'
+                  type='password'
+                  name='password'
+                  placeholder='Mật khẩu'
+                  value={password}
+                  onChange={(e) => onChange(setPassword, e)}
                 />
-                <span className="focus-input100" />
-                <span className="symbol-input100">
-                  <i className="fa fa-lock" aria-hidden="true" />
+                <span className='focus-input100' />
+                <span className='symbol-input100'>
+                  <i className='fa fa-lock' aria-hidden='true' />
                 </span>
               </div>
-              <div className="container-login100-form-btn">
-                <button className="login100-form-btn">Login</button>
+              <div className='container-login100-form-btn'>
+                <button className='login100-form-btn'>Đăng nhập</button>
               </div>
 
-              <div className="text-center p-t-136">
-                <a className="txt2" href="#">
-                  Create your Account
+              <div className='text-center p-t-136'>
+                <Link className='txt2' to='/register'>
+                  Chưa có tài khoản? Đăng ký
                   <i
-                    className="fa fa-long-arrow-right m-l-5"
-                    aria-hidden="true"
+                    className='fa fa-long-arrow-right m-l-5'
+                    aria-hidden='true'
                   />
-                </a>
+                </Link>
               </div>
             </form>
           </div>
